c93c85e15022d9d07474ab919acfa6f9cde3d89b
Everton J. Carpes <everton.carpes@gmail.com>
2013-11-14 08:56:23 -0200

    Initialize a Rails 4.0.1 application
      Ruby version              2.0.0 (i686-linux)
      RubyGems version          2.0.2
      Rack version              1.5
      Rails version             4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AciditCom::Application.config.secret_key_base = 'feaf150ce10e6b23dc38296628c59ba99dfe8cb5ae966a7934b3afac216394c45dd9deaceb5061dd4866c5c98325450aa073c2cb2be092a566b0093f8076e705'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AciditCom::Application.config.session_store :cookie_store, key: '_acidit_com_session'
